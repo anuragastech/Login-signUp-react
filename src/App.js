e81b69a5a77e50d22d366f783482dcb990bed186
Anurag <anuragraveendren98@gmail.com>
2024-08-04 03:40:25 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import Login from "./components/LoginSignup/login"
 import SignUp from "./components/LoginSignup/signup"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 class App extends Component {
   render() {
     return (
+
       <div className="App">
+              <BrowserRouter>
+
+      <Routes>
+      <Route path='/signup' element={<SignUp/>} /> 
+
    <Login/>
-   <SignUp/>
+   {/* <SignUp/> */}
+      </Routes>
+
+      </BrowserRouter>
       </div>
+
+
     );
   }
 }
